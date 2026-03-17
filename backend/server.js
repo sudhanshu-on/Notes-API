692c7195b6b6851bf516e1b0957d3b48e471ee0b
Sudhanshu Bagde <sudhanshubagde1234@gmail.com>
2026-03-17 14:39:02 +0530

    Connected Frontend to deployed backend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
 }));
 app.use(apiLimiter);
 
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://notes-api-production-cd96.up.railway.app",
 });
 
 // We add an "interceptor" to automatically attach the token if it exists
