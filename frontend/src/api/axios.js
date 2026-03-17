8bb54e47f60c39ef8852be13c85496bede4eefd0
Sudhanshu Bagde <sudhanshubagde1234@gmail.com>
2026-03-17 15:55:53 +0530

    changed baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://notes-api-production-cd96.up.railway.app/api",
+  baseURL: "https://notes-api-production-e602.up.railway.app/api",
 });
 
 // We add an "interceptor" to automatically attach the token if it exists
