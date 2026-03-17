75205025a904b03acc21d814c8c33fd1b44f9ced
Sudhanshu Bagde <sudhanshubagde1234@gmail.com>
2026-03-17 16:01:28 +0530

    changed baseURL

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://notes-api-iota-nine.vercel.app",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
